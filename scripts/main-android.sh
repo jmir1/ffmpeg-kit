a176cce37ceea4181f17e088d3007ebbc2634f3a
jmir1 <jhmiramon@gmail.com>
2022-08-12 20:57:16 +0000

    fix publishing

--- scripts/main-android.sh
+++ scripts/main-android.sh
@@ -45,7 +45,6 @@ for library in {1..50}; do
     ENABLED_LIBRARY=$(get_library_name $((library - 1)))
     enabled_library_list+=(${ENABLED_LIBRARY})
 
-    echo -e "INFO: Enabled library ${ENABLED_LIBRARY} will be built\n"
     echo -e "INFO: Enabled library ${ENABLED_LIBRARY} will be built\n" 1>>"${BASEDIR}"/build.log 2>&1
   fi
 done
@@ -199,7 +198,6 @@ while [ ${#enabled_library_list[@]} -gt $completed ]; do
         echo "${library}: already built"
       fi
     else
-      echo -e "INFO: Skipping $library, dependencies built=$run, already built=${!BUILD_COMPLETED_FLAG}\n"
       echo -e "INFO: Skipping $library, dependencies built=$run, already built=${!BUILD_COMPLETED_FLAG}\n" 1>>"${BASEDIR}"/build.log 2>&1
     fi
   done
@@ -253,4 +251,4 @@ else
   echo -e "\nffmpeg: skipped"
 fi
 
-echo -e "\nINFO: Completed build for ${ARCH} on API level ${API} at $(date)\n"# 1>>"${BASEDIR}"/build.log 2>&1
+echo -e "\nINFO: Completed build for ${ARCH} on API level ${API} at $(date)\n" 1>>"${BASEDIR}"/build.log 2>&1
